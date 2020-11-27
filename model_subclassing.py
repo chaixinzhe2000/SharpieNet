8e54106e7e523fae0e4b20f724a3b7f07e346b3a
elliotjinoohong <60581173+elliotjinoohong@users.noreply.github.com>
2020-11-27 17:04:09 -0500

    edits after i went through this code with chai

--- model_subclassing.py
+++ model_subclassing.py
@@ -1,16 +1,4 @@
 import tensorflow as tf
-import math
-import numpy as np
-
-from tensorflow.keras import datasets, layers, models
-from matplotlib import pyplot as plt
-
-
-# Common
-# def my_conv(model, in_channels, out_channels, kernel_size, bias=True):
-#     return tf.keras.layers.Conv2D(model.number_of_features, model.kernel_size, strides=(1, 1),
-#                                   padding=model.kernel_size // 2, activation="relu",
-#                                   kernel_initializer="Orthogonal")
 
 # ResBlock for our model:
 # no batch norm
@@ -22,16 +10,18 @@ class ResBlock(tf.keras.layers):
         self.conv1 = tf.keras.layers.Conv2D(filters, kernel_size, strides=(1, 1),
                                             padding="same",
                                             kernel_initializer="Orthogonal")
+        self.activation_fxn = tf.keras.activations.relu()
         self.conv2 = tf.keras.layers.Conv2D(filters, kernel_size, strides=(1, 1),
                                             padding="same",
                                             kernel_initializer="Orthogonal")
-        self.activation_fxn = tf.keras.activations.relu()
+
         # self.res_factor = res_factor
 
     def call(self, model, input_tensor):
         x = self.conv1(input_tensor)
-        x = self.conv2(x)
         x = self.activation_fxn(x)
+        x = self.conv2(x)
+        x = tf.multiply(x, model.res_scaling)
         x += input_tensor
         return x
 
@@ -49,13 +39,14 @@ class Upsampler(tf.keras.layers):
                                                 model.kernel_size, strides=(1, 1),
                                                 padding="same",
                                                 kernel_initializer="Orthogonal")
-            # CAN I DEFINE A FUNCTION LIKE THIS?
+            # TODO: CAN I DEFINE A FUNCTION LIKE THIS?
             self.pixel_shuffle = tf.nn.depth_to_space
         else:
             raise NotImplementedError
 
     def call(self, input_tensor):
         x = self.conv1(input_tensor)
+        # TODO: check if this is the right "block_size" does it want 3 or 9
         x = self.pixel_shuffle(x, self.model_scaling_factor)
         return x
 
@@ -67,13 +58,13 @@ class EDSR_super:
         self.number_of_resblocks = 32
         self.number_of_features = 256
         self.kernel_size = 3
-        # self.res_factor = 0.1
+        self.res_scaling = 0.1
         self.scaling_factor = 3
         self.final_output_channels = 3
 
         # from keras tutorial
-        self.upscale_factor = upscale_factor
-        self.channels = channels
+        # self.upscale_factor = upscale_factor
+        # self.channels = channels
         self.input_shape = input_shape
 
         # model
@@ -83,12 +74,14 @@ class EDSR_super:
                                    padding=self.kernel_size // 2,
                                    kernel_initializer="Orthogonal", input_shape=self.input_shape)(inputs)
         # body
+        # TODO: Not sure if we can for loop like this
         for i in range(self.number_of_resblocks):
             x = ResBlock(self, self.kernel_size, self.number_of_features).call(model=self, input_tensor=x)
         # tail
         x = Upsampler(model=self, number_of_features=self.number_of_features).call(x)
         x = tf.keras.layers.Conv2D(self.final_output_channels, self.kernel_size, strides=(1, 1),
                                    padding="same", kernel_initializer="Orthogonal")(x)
+
         self.EDSR_model = tf.keras.Model(inputs, x)
         self.EDSR_model.summary()
 
@@ -98,38 +91,3 @@ class EDSR_super:
     def test(self):
         pass
 
-# class EDSR_class(tf.keras.Model):
-#     def __init__(self, upscale_factor, channels, input_shape):
-#         super(EDSR_class, self).__init__()
-#         self.upscale_factor = upscale_factor
-#         self.channels = channels
-#         self.input_shape = input_shape
-#
-#         # define inputs and outputs for keras model using functional API
-#         inputs = tf.keras.Input(shape=self.input_shape)
-#         x = tf.keras.layers.Conv2D(64, 5, strides=(1, 1), padding="same", activation="relu",
-#                                    kernel_initializer="Orthogonal", input_shape=self.input_shape)(inputs)
-#         x = tf.keras.layers.Conv2D(64, 3, strides=(1, 1), padding="same", activation="relu",
-#                                    kernel_initializer="Orthogonal", input_shape=self.input_shape)(x)
-#         x = tf.keras.layers.Conv2D(32, 3, strides=(1, 1), padding="same", activation="relu",
-#                                    kernel_initializer="Orthogonal", input_shape=self.input_shape)(x)
-#         x = tf.keras.layers.Conv2D(self.channels * (self.upscale_factor ** 2), 3, padding="same", activation="relu",
-#                                    kernel_initializer="Orthogonal", input_shape=self.input_shape)(x)
-#         outputs = tf.nn.depth_to_space(x, upscale_factor)
-#
-#         # initialize model
-#         self.model = tf.keras.Model(inputs=inputs, outputs=outputs)
-#
-#     def train(self, training_images, training_labels, loss_function, optimizer, number_of_batches, number_of_epochs):
-#         # compile model
-#         self.model.compile(optimizer=optimizer, loss=loss_function)
-#         # fit model
-#         batch_size = tf.floor(tf.shape(training_images)[0], number_of_batches)
-#         self.model.fit(training_images, training_labels, batch_size, number_of_epochs)
-#
-#     def test_evaluate_and_output_images(self, test_images, test_labels):
-#         predictions = self.model.predict(test_images)
-#
-#         # spit out images comparing test_images with test_labels
-#
-#         # evaluate model against psnr or smth like that and return tuple with those measur
