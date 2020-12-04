52905284c3084d4136008528817a5e5bc29a0ff7
elliotjinoohong <60581173+elliotjinoohong@users.noreply.github.com>
2020-12-04 00:27:05 -0500

    delete redundant datasets

--- predict_from_saved_model.py
+++ predict_from_saved_model.py
@@ -11,7 +11,7 @@ def main():
     epoch = 100
 
     dirname = os.path.dirname(__file__)
-    model = tf.keras.models.load_model("saved_models/TRIAL5668_9177991899817384-RB_8-FEATS_64-VGGOUT_12-BSZ_25-EPOCH_20-LOSS_6537.1.hdf5")
+    model = tf.keras.models.load_model("saved_models/TRIAL7384-RB_8-FEATS_64-VGGOUT_12-BSZ_25-EPOCH_02-LOSS_23698.4.hdf5")
     model.summary()
     test_path = os.path.join(dirname, "div2k_and_BSDS500_dataset/test")
 
